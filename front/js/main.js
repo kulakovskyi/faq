8479f6fbbf51ec049955cc4c93b8f50ef1144c45
kukakovskyi <a.kulakovskyi@favbet.com>
2022-08-09 11:10:59 +0300

    languagesupport

--- front/js/main.js
+++ front/js/main.js
@@ -1,12 +1,43 @@
 const btnShow = document.querySelector('.question__btn');
 const secondList = document.querySelector('.faq__list-not_show');
 
+let userLang = navigator.language || navigator.userLanguage; ;
+console.log(userLang);
+
+let userBtnHideArr = ['hide', 'Скрыть', 'Приховати', 'Ocultar', 'Ocultar'];
+let userBtnShowArr = ['show more','Показать больше', 'Показати більше', 'Mostrar más', 'Mostrar más'];
+
+let userBtnHide = '';
+let userBtnShow = '';
+
+if(userLang == 'en-US'){
+    userBtnHide = userBtnHideArr[0];
+    userBtnShow = userBtnShowArr[0];
+}
+if(userLang == 'ru'){
+    userBtnHide = userBtnHideArr[1];
+    userBtnShow = userBtnShowArr[1];
+}
+if(userLang == 'uk-UA'){
+    userBtnHide = userBtnHideArr[2];
+    userBtnShow = userBtnShowArr[2];
+}
+if(userLang == 'es'){
+    userBtnHide = userBtnHideArr[3];
+    userBtnShow = userBtnShowArr[3];
+}
+if(userLang == 'pt'){
+    userBtnHide = userBtnHideArr[4];
+    userBtnShow = userBtnShowArr[4];
+}
+
+
 btnShow.addEventListener('click', ()=>{
     secondList.classList.toggle('_open');
     if(secondList.classList.contains('_open')){
-        btnShow.innerHTML = 'hide'
+        btnShow.innerHTML = userBtnHide;
     } else {
-        btnShow.innerHTML = 'show more'
+        btnShow.innerHTML = userBtnShow;
     }
 })
 
